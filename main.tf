aa32c15b39f24224e0d6de6971557910cb0dd22d
jgolbez <42747999+jgolbez@users.noreply.github.com>
2023-09-24 17:52:47 -0700

    11 s3 bucket for terraform (#12)
    * removed sts get caller identity from aws install script
    
    * added aws s3 bucket code
    
    * #11 added aws s3 bucket code
    
    * #11 updated Readme

--- main.tf
+++ main.tf
@@ -4,17 +4,27 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+      aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 provider "random" {
   # Configuration options
 }
 
+
 resource "random_string" "bucket_name" {
   length           = 16
   special          = false
+  upper = false
 }
 
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = random_string.bucket_name.result
+}
 output "random_bucket_name" {
     value = random_string.bucket_name.result
 }
+#
