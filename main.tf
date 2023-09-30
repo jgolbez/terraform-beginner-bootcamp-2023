ec2f8c6d41bcaa4b1d709a0f10bdbef4381b1be9
jgolbez <42747999+jgolbez@users.noreply.github.com>
2023-09-30 11:45:05 -0700

    #27 set up static website hosting and uploaded files via TF (#28)

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  cloud {
-    organization = "FrustratedIncorporated"
-    workspaces {
-      name = "Terra-house"
-    }
-  }
+  # cloud {
+  #   organization = "FrustratedIncorporated"
+  #   workspaces {
+  #     name = "Terra-house"
+  #   }
+  #}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -22,4 +22,6 @@ module "terrahouse_aws" {
   source                 = "./modules/terrahouse_aws"
   user_uuid              = var.user_uuid
   s3_website_bucket_name = var.s3_website_bucket_name
+  index_html_filepath    = var.index_html_filepath
+  error_html_filepath    = var.error_html_filepath
 }
--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -21,3 +21,23 @@ variable "s3_website_bucket_name" {
     error_message = "S3 bucket name must be between 3 and 63 characters, containing only lowercase letters, numbers, hyphens, and periods, and it cannot start or end with a hyphen or period."
   }
 }
+
+variable "index_html_filepath" {
+    description = "File path to index.html"
+    type = string
+
+    validation {
+        condition = fileexists(var.index_html_filepath)
+        error_message = "The provided path for index.html does not exist"
+    }
+}
+
+variable "error_html_filepath" {
+    description = "File path to error.html"
+    type = string
+
+    validation {
+        condition = fileexists(var.error_html_filepath)
+        error_message = "The provided path for error.html does not exist"
+    }
+}
