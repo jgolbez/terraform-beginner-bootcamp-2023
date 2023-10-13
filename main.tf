b1d7c2a0d202472b6efb72fdbbbf853634ea643e
jgolbez <42747999+jgolbez@users.noreply.github.com>
2023-10-13 05:47:59 -0700

    39 terratowns provider (#42)
    * #39 added golang code, custom provider
    
    * #39 fixed bash script
    
    * #39 fixed minor erors with TF providr and debug logging

--- main.tf
+++ main.tf
@@ -10,11 +10,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.17.0"
     }
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
   }
 }
 
 provider "aws" {
   # Configuration options
+}
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
+  token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 
 
